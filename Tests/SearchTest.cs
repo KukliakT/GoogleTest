9ab37f21ede39b30142766719ab9cf3e7a197981
KukliakT <kuklyak.tarass@gmail.com>
2019-10-05 19:00:46 +0300

    Added first test class 'SearchTest' and SimpleSearchTest()

---
+++ Tests/SearchTest.cs
@@ -0,0 +1,25 @@
+﻿using GoogleTest.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleTest.Tests
+{
+    [TestFixture]
+    class SearchTest : TestRunner
+    {
+        [TestCase]
+        public void SimpleSearchTest()
+        {
+            HomePage homePage = LoadApplication();
+
+            homePage.searchComponent.Search("Allo");
+            Thread.Sleep(4000);   //Only for presentation
+            
+        }
+    }
+}
